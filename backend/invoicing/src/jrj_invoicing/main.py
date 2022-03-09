2af19986e36a681593694f67755e7e0f0b596423
Humberto Ramírez <humberto.rjdh@gmail.com>
2022-03-09 12:07:39 -0600

    Add main module

---
+++ backend/invoicing/src/jrj_invoicing/main.py
@@ -0,0 +1,13 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI(title="JRJ Invoicing API")
+
+
+@app.get("/")
+async def root():
+    return {"status": "Up!"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app="main:app", host="0.0.0.0", port=8000, reload=True)
