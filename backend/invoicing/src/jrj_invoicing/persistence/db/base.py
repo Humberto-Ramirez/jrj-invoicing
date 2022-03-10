410ab9d789eb0639dda65cb5dbcd2003eea0bffe
Humberto Ramírez <humberto.rjdh@gmail.com>
2022-03-10 14:38:18 -0600

    Add material model

--- backend/invoicing/src/jrj_invoicing/persistence/db/base.py
+++ backend/invoicing/src/jrj_invoicing/persistence/db/base.py
@@ -2,3 +2,4 @@
 # imported by Alembic
 from jrj_invoicing.persistence.db.base_class import Audit, Base
 from jrj_invoicing.persistence.models.product import Product
+from jrj_invoicing.persistence.models.material import Material
---
+++ backend/invoicing/src/jrj_invoicing/persistence/models/material.py
@@ -0,0 +1,9 @@
+from jrj_invoicing.persistence.db.base_class import Audit, Base
+from sqlalchemy import Column, Integer, String, Float, Date, Boolean
+
+
+class Material(Base, Audit):
+    sku = Column(String, primary_key=True, index=True)
+    name = Column(String, index=True, nullable=False)
+    description = Column(String, nullable=False)
+    price = Column(Float)
