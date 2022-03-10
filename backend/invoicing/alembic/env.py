38cc4e6247f1aba39ccc56039dc4a6402207bce3
Humberto Ramírez <humberto.rjdh@gmail.com>
2022-03-10 11:59:28 -0600

    Reformat alembic env script

--- backend/invoicing/alembic/env.py
+++ backend/invoicing/alembic/env.py
@@ -23,10 +23,12 @@ fileConfig(config.config_file_name)
 
 target_metadata = Base.metadata
 
+
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
+
 
 def get_url():
     user = os.getenv("POSTGRES_USER", "postgres")
