b456d000252d66831d5e75965fb5f49b8e9046ff
Humberto Ramírez <humberto.rjdh@gmail.com>
2022-03-12 17:55:04 -0600

    Add Orders endpoint

--- backend/invoicing/src/jrj_invoicing/api/api.py
+++ backend/invoicing/src/jrj_invoicing/api/api.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
-from .endpoints import materials
+
+from .endpoints import materials, orders
 
 api_router = APIRouter()
 
 api_router.include_router(materials.router, prefix="/materials", tags=["materials"])
+api_router.include_router(orders.router, prefix="/orders", tags=["orders"])
--- backend/invoicing/src/jrj_invoicing/api/endpoints/orders.py
+++ backend/invoicing/src/jrj_invoicing/api/endpoints/orders.py
@@ -1,28 +1,26 @@
 from typing import Any, List
 
 from fastapi import APIRouter, Depends
-from fastapi.encoders import jsonable_encoder
 from jrj_invoicing import schemas
 from jrj_invoicing.api import deps
 from jrj_invoicing.persistence import crud
-from loguru import logger
 from sqlalchemy.orm import Session
 
 router = APIRouter()
 
 
-@router.get("/", response_model=List[schemas.MaterialEntity])
-def read_materials(
+@router.get("/", response_model=List[schemas.OrderEntity])
+def read_orders(
         db: Session = Depends(deps.get_db),
         offset: int = 0,
         limit: int = 100
 ) -> Any:
     """
-    Retrieve materials
+    Retrieve orders
     :param db:
     :param offset:
     :param limit:
     :return:
     """
-    materials = crud.material.get_multi(db, skip=offset, limit=limit)
-    return materials
+    orders = crud.order.get_multi(db, skip=offset, limit=limit)
+    return orders
