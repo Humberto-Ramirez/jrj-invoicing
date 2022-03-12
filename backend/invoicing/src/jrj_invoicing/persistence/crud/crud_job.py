de1c8b20672b67cd1ef507a019f8ec03c9783ab1
Humberto Ramírez <humberto.rjdh@gmail.com>
2022-03-12 17:59:30 -0600

    Fetch invoice orders on order-crud

--- backend/invoicing/src/jrj_invoicing/persistence/crud/crud_job.py
+++ backend/invoicing/src/jrj_invoicing/persistence/crud/crud_job.py
@@ -6,6 +6,8 @@ from sqlalchemy.orm import Session
 
 
 class CRUDJob(CRUDBase[Job, JobEntity, JobEntity]):
+    def get_by_invoice(self, db: Session, invoice_id: int):
+        return db.query(self.model).filter(self.model.invoice_id == invoice_id).all()
 
     def create_active(self, db: Session, *, obj_in: JobEntity):
         obj_in_data = jsonable_encoder(obj_in)
