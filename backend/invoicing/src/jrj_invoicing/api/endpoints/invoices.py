1445025f67d865ee49b61da56074e30b776be20d
Humberto Ramírez <humberto.rjdh@gmail.com>
2022-03-12 18:41:51 -0600

    Add create-invoice service

--- backend/invoicing/src/jrj_invoicing/api/endpoints/invoices.py
+++ backend/invoicing/src/jrj_invoicing/api/endpoints/invoices.py
@@ -24,3 +24,19 @@ def read_invoices(
     """
     invoices = crud.invoice.get_multi(db, skip=offset, limit=limit)
     return invoices
+
+
+@router.post("/add", response_model=schemas.InvoiceEntity)
+def create_invoice(
+        *,
+        db: Session = Depends(deps.get_db),
+        invoice: schemas.InvoiceDto
+) -> Any:
+    """
+    Register a new Invoice
+    :param db:
+    :param invoice:
+    :return:
+    """
+    invoice_db = crud.invoice.create(db, obj_in=invoice)
+    return invoice_db
