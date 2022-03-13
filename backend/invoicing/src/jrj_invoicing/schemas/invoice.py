3e2ef34db8db5ae3f0c4440c42d62032bc4f248a
Humberto Ramírez <humberto.rjdh@gmail.com>
2022-03-12 18:24:31 -0600

    Update invoices endpoint

--- backend/invoicing/src/jrj_invoicing/schemas/invoice.py
+++ backend/invoicing/src/jrj_invoicing/schemas/invoice.py
@@ -2,7 +2,7 @@ import datetime
 from typing import Optional, List
 
 from pydantic import BaseModel
-from .job import JobDto
+from .job import JobEntity
 
 
 class InvoiceBase(BaseModel):
@@ -11,12 +11,13 @@ class InvoiceBase(BaseModel):
 
 class InvoiceDto(InvoiceBase):
     sw_active: Optional[bool] = None
-    jobs: Optional[List[JobDto]] = []
+    jobs: Optional[List[JobEntity]] = []
 
 
 class InvoiceEntity(InvoiceBase):
     sw_active: Optional[bool] = True
     report_date: Optional[datetime.date] = datetime.date.today()
+    jobs: List[JobEntity] = []
 
     class Config:
         orm_mode = True
