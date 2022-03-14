a71e15862c88b234cb866cb679733df0d71a261b
Humberto Ramírez <humberto.rjdh@gmail.com>
2022-03-14 14:11:34 -0600

    Add create-job endpoint

--- backend/invoicing/src/jrj_invoicing/api/endpoints/jobs.py
+++ backend/invoicing/src/jrj_invoicing/api/endpoints/jobs.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, Depends
 from jrj_invoicing import schemas
 from jrj_invoicing.api import deps
 from jrj_invoicing.persistence import crud
+from loguru import logger
 from sqlalchemy.orm import Session
 
 router = APIRouter()
@@ -24,3 +25,23 @@ def read_jobs(
     """
     jobs = crud.job.get_multi(db, skip=offset, limit=limit)
     return jobs
+
+
+@router.post("/add", response_model=List[schemas.JobEntity])
+def create_jobs(
+        *,
+        db: Session = Depends(deps.get_db),
+        jobs: List[schemas.JobDto],
+) -> Any:
+    """
+    Register jobs
+    :param db:
+    :param jobs:
+    :return:
+    """
+    saved_jobs = []
+    for item in jobs:
+        saved_item = crud.job.create(db, obj_in=item)
+        logger.debug(f"Job saved: {schemas.JobEntity.from_orm(saved_item)}")
+        saved_jobs.append(saved_item)
+    return saved_jobs
