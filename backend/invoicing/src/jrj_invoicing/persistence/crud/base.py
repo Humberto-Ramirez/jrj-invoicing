0c93ccd6318cf1c88e7b5b944f4955f0d3d92305
Humberto Ramírez <humberto.rjdh@gmail.com>
2022-03-14 15:10:11 -0600

    Add update operation on base crud

--- backend/invoicing/src/jrj_invoicing/persistence/crud/base.py
+++ backend/invoicing/src/jrj_invoicing/persistence/crud/base.py
@@ -1,9 +1,10 @@
-from typing import TypeVar, Generic, Type, Any, Optional, List
+from typing import TypeVar, Generic, Type, Any, Optional, List, Union, Dict
 
 from fastapi.encoders import jsonable_encoder
 from jrj_invoicing.persistence.db.base import Base
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
+from loguru import logger
 
 ModelType = TypeVar("ModelType", bound=Base)
 CreateSchemaType = TypeVar("CreateSchemaType", bound=BaseModel)
@@ -33,6 +34,26 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
     def create(self, db: Session, *, obj_in: CreateSchemaType) -> ModelType:
         obj_in_data = jsonable_encoder(obj_in)
         db_obj = self.model(**obj_in_data)  # type: ignore
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
+
+    def update(self,
+               db: Session,
+               *,
+               db_obj: ModelType,
+               obj_in: Union[UpdateSchemaType, Dict[str, Any]]
+               ) -> ModelType:
+        obj_data = jsonable_encoder(db_obj)
+        if isinstance(obj_in, dict):
+            update_data = obj_in
+        else:
+            update_data = obj_in.dict(exclude_unset=True)
+        for field in obj_data:
+            if field in update_data:
+                logger.info(f"setting {field}: {update_data[field]}")
+                setattr(db_obj, field, update_data[field])
         db.add(db_obj)
         db.commit()
         db.refresh(db_obj)
