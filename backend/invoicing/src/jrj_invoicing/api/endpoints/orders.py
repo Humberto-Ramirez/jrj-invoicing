1a70151b9c239a145f59e2750793e99eca00e18b
Humberto Ramírez <humberto.rjdh@gmail.com>
2022-03-14 17:11:37 -0600

    Add service to create orders

--- backend/invoicing/src/jrj_invoicing/api/endpoints/orders.py
+++ backend/invoicing/src/jrj_invoicing/api/endpoints/orders.py
@@ -5,6 +5,7 @@ from jrj_invoicing import schemas
 from jrj_invoicing.api import deps
 from jrj_invoicing.persistence import crud
 from sqlalchemy.orm import Session
+from loguru import logger
 
 router = APIRouter()
 
@@ -24,3 +25,23 @@ def read_orders(
     """
     orders = crud.order.get_multi(db, skip=offset, limit=limit)
     return orders
+
+
+@router.post("/", response_model=List[schemas.OrderEntity])
+def create_orders(
+        db: Session = Depends(deps.get_db),
+        *,
+        orders: List[schemas.OrderDto],
+        job_id: int,
+) -> Any:
+    """
+    Create orders
+    :param job_id:
+    :param db:
+    :param orders:
+    :return:
+    """
+    saved_orders = crud.order.create_orders(db, objs_in=orders, job=job_id)
+    for item in saved_orders:
+        logger.info(f"Saved order: {schemas.OrderEntity.from_orm(item)}")
+    return saved_orders
